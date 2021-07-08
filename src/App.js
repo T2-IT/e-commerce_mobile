b93d686c9e2821369bd49df17ac94e5d6ba66924
hmZ <duduhammes@hotmail.com>
2021-07-08 17:46:23 -0300

    vasco

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, SafeAreaView} from 'react-native';
+import Login from './pages/Login/index';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <Login />
+    </SafeAreaView>
+  );
+}
+
+export default App;
